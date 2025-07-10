2a25144930d1bc1f9e72ebe16924ec804a150a05
davidmarianosantos <david.drinks@hotmail.com>
2025-07-10 20:39:23 -0300

    Documentação e queries

--- Dados_Climaticos/queryviews.py
+++ Dados_Climaticos/queryviews.py
@@ -10,6 +10,7 @@ from django.utils import timezone
 from datetime import datetime
 from utils import is_valid_uuid, get_dispositivo
 import numpy as np
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiTypes, OpenApiExample
 
 #Ultimo dado enviado por um dispositivo
 class UltimoDadoView(APIView):
@@ -99,19 +100,120 @@ class QueryMediaUnicaView(APIView):
             'dados': list(query)
         })
     
-class DadoClimaticoPorPeriodoView(APIView):
+@extend_schema(
+    description="Retorna os dados climáticos dos dispositivos no período especificado.",
+    parameters=[
+        OpenApiParameter(
+            name='dispositivos',
+            type=OpenApiTypes.STR,
+            location=OpenApiParameter.QUERY,
+            required=True,
+            description='Lista de IDs dos dispositivos (ex: dispositivos=1&dispositivos=2)'
+        ),
+        OpenApiParameter(
+            name='inicio',
+            type=OpenApiTypes.DATETIME,
+            location=OpenApiParameter.QUERY,
+            required=True,
+            description='Data/hora de início no formato ISO 8601 (ex: 2025-03-31T07:54:57)'
+        ),
+        OpenApiParameter(
+            name='fim',
+            type=OpenApiTypes.DATETIME,
+            location=OpenApiParameter.QUERY,
+            required=True,
+            description='Data/hora de fim no formato ISO 8601 (ex: 2025-04-01T07:54:57)'
+        ),
+    ],
+    responses={
+        200: OpenApiTypes.OBJECT,
+        400: OpenApiTypes.OBJECT,
+    },
+    examples=[
+        OpenApiExample(
+            'Parâmetros obrigatórios ausentes',
+            value={
+                "status": 400,
+                "msg": 'Parâmetros "dispositivos", "inicio" e "fim" são obrigatórios.'
+            },
+            response_only=True,
+            status_codes=['400']
+        ),
+        OpenApiExample(
+            'IDs inválidos em dispositivos',
+            value={
+                "status": 400,
+                "msg": 'IDs inválidos em "dispositivos".'
+            },
+            response_only=True,
+            status_codes=['400']
+        ),
+        OpenApiExample(
+            'Formato inválido de datas',
+            value={
+                "status": 400,
+                "msg": 'Datas "inicio" e "fim" devem estar no formato ISO 8601.'
+            },
+            response_only=True,
+            status_codes=['400']
+        ),
+        OpenApiExample(
+            'Sucesso com dados climáticos',
+            value={
+                "status": 200,
+                "msg": "Dados climáticos dos dispositivos no período 2025-03-31T07:00:00 a 2025-03-31T09:00:00.",
+                "dados_climaticos": [
+                    {
+                        "id": 1,
+                        "dispositivo": 1,
+                        "data": "2025-03-31T07:54:57-03:00",
+                        "temperatura": 30.2,
+                        "umidade": 0.8,
+                        "precipitacao": 0.2,
+                        "velocidade_vento": 10.5
+                    },
+                    {
+                        "id": 2,
+                        "dispositivo": 1,
+                        "data": "2025-03-31T08:54:57-03:00",
+                        "temperatura": 20.2,
+                        "umidade": 0.8,
+                        "precipitacao": 0.2,
+                        "velocidade_vento": 5.5
+                    }
+                ]
+            },
+            response_only=True,
+            status_codes=['200']
+        ),
+        OpenApiExample(
+            'Nenhum dado encontrado',
+            value={
+                "status": 200,
+                "msg": "Nenhum dado climático encontrado para os dispositivos no período 2025-03-31T07:00:00 a 2025-03-31T09:00:00.",
+                "dados_climaticos": []
+            },
+            response_only=True,
+            status_codes=["200"]
+        )
+    ]
+)
 
+class DadoClimaticoPorPeriodoView(APIView):
     def get(self, request):
+        # Captura os parâmetros da URL: lista de dispositivos, data de início e data de fim
         dispositivos_ids = request.query_params.getlist('dispositivos')
         inicio_str = request.query_params.get('inicio')
         fim_str = request.query_params.get('fim')
 
+        # Valida se todos os parâmetros foram fornecidos
         if not dispositivos_ids or not inicio_str or not fim_str:
             return Response({
                 'status': 400,
                 'msg': 'Parâmetros "dispositivos", "inicio" e "fim" são obrigatórios.'
             }, status=400)
 
+        # Tenta converter os IDs dos dispositivos para inteiros
         try:
             dispositivos_ids = [int(i) for i in dispositivos_ids]
         except ValueError:
@@ -120,6 +222,7 @@ class DadoClimaticoPorPeriodoView(APIView):
                 'msg': 'IDs inválidos em "dispositivos".'
             }, status=400)
 
+        # Tenta converter as strings de data para objetos datetime conscientes de fuso horário
         try:
             inicio = timezone.make_aware(datetime.fromisoformat(inicio_str))
             fim = timezone.make_aware(datetime.fromisoformat(fim_str))
@@ -129,38 +232,161 @@ class DadoClimaticoPorPeriodoView(APIView):
                 'msg': 'Datas "inicio" e "fim" devem estar no formato ISO 8601.'
             }, status=400)
 
+        # Filtra os dados climáticos no intervalo de tempo e pelos dispositivos
         dados = DadoClimatico.objects.filter(
             dispositivo_id__in=dispositivos_ids,
             time__range=(inicio, fim)
         )
 
+         # Retorno específico se não houver dados encontrados
+        if not dados.exists():
+            return Response({
+                'status': 200,
+                'msg': f'Nenhum dado climático encontrado para os dispositivos no período {inicio_str} a {fim_str}.',
+                'dados_climaticos': []
+            })
+        
+        # Serializa os dados para retorno em JSON
         serializer = DadoClimaticoSerializer(dados, many=True)
 
+        # Retorna a resposta com status e dados encontrados
         return Response({
             'status': 200,
             'msg': f'Dados climáticos dos dispositivos no período {inicio_str} a {fim_str}.',
             'dados_climaticos': serializer.data
         })
 
-    
+  
+@extend_schema(
+    description="Gera um histograma dos valores de temperatura ou umidade para os dispositivos informados, dentro de um intervalo de tempo.",
+    parameters=[
+        OpenApiParameter(
+            name='dispositivos',
+            type=OpenApiTypes.STR,
+            location=OpenApiParameter.QUERY,
+            required=True,
+            description='Lista de IDs dos dispositivos (ex: dispositivos=1&dispositivos=2)'
+        ),
+        OpenApiParameter(
+            name='campo',
+            type=OpenApiTypes.STR,
+            location=OpenApiParameter.QUERY,
+            required=True,
+            description='Campo a ser analisado: "temperatura" ou "umidade"'
+        ),
+        OpenApiParameter(
+            name='inicio',
+            type=OpenApiTypes.DATETIME,
+            location=OpenApiParameter.QUERY,
+            required=True,
+            description='Data/hora inicial (ex: 2025-03-31T07:54:57)'
+        ),
+        OpenApiParameter(
+            name='fim',
+            type=OpenApiTypes.DATETIME,
+            location=OpenApiParameter.QUERY,
+            required=True,
+            description='Data/hora final (ex: 2025-04-01T07:54:57)'
+        ),
+        OpenApiParameter(
+            name='bins',
+            type=OpenApiTypes.INT,
+            location=OpenApiParameter.QUERY,
+            required=False,
+            description='Quantidade de divisões (bins) no histograma. Padrão: 10'
+        ),
+    ],
+    responses={
+        200: OpenApiTypes.OBJECT,
+        400: OpenApiTypes.OBJECT
+    },
+     examples=[
+        OpenApiExample(
+            'Sucesso com dados',
+            value={
+                "status": 200,
+                "msg": "Histograma de temperatura para dispositivos [1, 2] entre 2025-03-31T07:54:57 e 2025-04-01T07:54:57.",
+                "histograma": [
+                    {"bin_inicio": 20.0, "bin_fim": 21.0, "quantidade": 10},
+                    {"bin_inicio": 21.0, "bin_fim": 22.0, "quantidade": 15},
+                    {"bin_inicio": 22.0, "bin_fim": 23.0, "quantidade": 5}
+                ]
+            },
+            response_only=True,
+            status_codes=['200']
+        ),
+        OpenApiExample(
+            'Sucesso sem dados',
+            value={
+                "status": 200,
+                "msg": "Nenhum dado encontrado no período para os dispositivos.",
+                "histograma": []
+            },
+            response_only=True,
+            status_codes=['200']
+        ),
+        OpenApiExample(
+            'Parâmetros inválidos',
+            value={
+                "status": 400,
+                "msg": "Parâmetros inválidos."
+            },
+            response_only=True,
+            status_codes=['400']
+        ),
+        OpenApiExample(
+            'Dispositivos ausentes',
+            value={
+                "status": 400,
+                "msg": '"dispositivos" deve ser uma lista de IDs.'
+            },
+            response_only=True,
+            status_codes=['400']
+        ),
 
+        OpenApiExample(
+            'Campo inválido',
+            value={
+                "status": 400,
+                "msg": 'Campo deve ser "temperatura" ou "umidade".'
+            },
+            response_only=True,
+            status_codes=['400']
+        ),
+
+        OpenApiExample(
+            'Datas ausentes',
+            value={
+                "status": 400,
+                "msg": 'Parâmetros "inicio" e "fim" são obrigatórios.'
+            },
+            response_only=True,
+            status_codes=['400']
+        )
+    ]
+)
 class HistogramaPorDispositivosView(APIView):
     def get(self, request):
+        # Captura os parâmetros da URL
         dispositivos_ids = request.query_params.getlist('dispositivos')
         campo = request.query_params.get('campo')
         inicio_str = request.query_params.get('inicio')
         fim_str = request.query_params.get('fim')
-        bins = request.query_params.get('bins', 10)
+        bins = request.query_params.get('bins', 10)  # valor padrão é 10
 
+        # Validação dos dispositivos
         if not dispositivos_ids:
             return Response({'status': 400, 'msg': '"dispositivos" deve ser uma lista de IDs.'}, status=400)
 
+        # Validação do campo analisado
         if campo not in ['temperatura', 'umidade']:
             return Response({'status': 400, 'msg': 'Campo deve ser "temperatura" ou "umidade".'}, status=400)
 
+        # Validação das datas
         if not inicio_str or not fim_str:
             return Response({'status': 400, 'msg': 'Parâmetros "inicio" e "fim" são obrigatórios.'}, status=400)
 
+        # Conversão e validação dos tipos
         try:
             dispositivos_ids = [int(i) for i in dispositivos_ids]
             inicio = timezone.make_aware(datetime.fromisoformat(inicio_str))
@@ -169,6 +395,7 @@ class HistogramaPorDispositivosView(APIView):
         except Exception:
             return Response({'status': 400, 'msg': 'Parâmetros inválidos.'}, status=400)
 
+        # Consulta dos valores do campo específico, ignorando nulos
         valores_qs = DadoClimatico.objects.filter(
             dispositivo_id__in=dispositivos_ids,
             time__range=(inicio, fim),
@@ -176,11 +403,18 @@ class HistogramaPorDispositivosView(APIView):
 
         valores = list(valores_qs)
 
+        # Retorno caso não haja dados
         if not valores:
-            return Response({'status': 200, 'msg': 'Nenhum dado encontrado no período para os dispositivos.', 'histograma': []})
+            return Response({
+                'status': 200,
+                'msg': 'Nenhum dado encontrado no período para os dispositivos.',
+                'histograma': []
+            })
 
+        # Geração do histograma com numpy
         counts, bin_edges = np.histogram(valores, bins=bins)
 
+        # Formata os dados para o retorno
         histograma = []
         for i in range(len(counts)):
             histograma.append({
@@ -189,6 +423,7 @@ class HistogramaPorDispositivosView(APIView):
                 'quantidade': int(counts[i])
             })
 
+        # Resposta final com status, mensagem e histograma
         return Response({
             'status': 200,
             'msg': f'Histograma de {campo} para dispositivos {dispositivos_ids} entre {inicio_str} e {fim_str}.',
--- Dispositivo/queryviews.py
+++ Dispositivo/queryviews.py
@@ -6,52 +6,215 @@ from rest_framework import status
 from Dispositivo.models import Dispositivo
 from Dispositivo.serializer import DispositivoSerializer, DispositivoSimplesSerializer
 from django.contrib.gis.measure import Distance as D
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiTypes, OpenApiExample, inline_serializer
+from rest_framework import serializers
 
+@extend_schema(
+    description=(
+        "Retorna os dispositivos localizados dentro de um raio (em km) a partir de uma coordenada "
+        "geográfica (latitude e longitude). Inclui a distância individual de cada dispositivo até o ponto de referência."
+    ),
+    parameters=[
+        OpenApiParameter(
+            name='latitude',
+            type=OpenApiTypes.FLOAT,
+            location=OpenApiParameter.QUERY,
+            required=True,
+            description='Latitude do ponto de referência (ex: -15.7801)'
+        ),
+        OpenApiParameter(
+            name='longitude',
+            type=OpenApiTypes.FLOAT,
+            location=OpenApiParameter.QUERY,
+            required=True,
+            description='Longitude do ponto de referência (ex: -47.9292)'
+        ),
+        OpenApiParameter(
+            name='raio',
+            type=OpenApiTypes.FLOAT,
+            location=OpenApiParameter.QUERY,
+            required=True,
+            description='Raio de busca em quilômetros (ex: 5)'
+        ),
+    ],
+    responses={
+        200: OpenApiTypes.OBJECT,
+        400: OpenApiTypes.OBJECT
+    },
+    examples=[
+        OpenApiExample(
+            'Dispositivos encontrados',
+            response_only=True,
+            status_codes=['200'],
+            value={
+                "status": 200,
+                "msg": "Dispositivos encontrados num raio de 5000.0 km.",
+                "dispositivos": [
+                    {
+                        "id": 1,
+                        "type": "Feature",
+                        "geometry": "SRID=4326;POINT (-45.6789 -12.34567)",
+                        "properties": {
+                            "token": "ad9da4ba-9f2f-4f06-83fe-485c6b24f03c",
+                            "descricao": "Estação cacau"
+                        },
+                        "distancia_km": 451.041
+                    },
+                    {
+                        "id": 2,
+                        "type": "Feature",
+                        "geometry": "SRID=4326;POINT (-8.6789 -8.303)",
+                        "properties": {
+                            "token": "3d48fe06-1a2a-42f3-bc9c-304ea8a2c72b",
+                            "descricao": "Estação ceplac"
+                        },
+                        "distancia_km": 4345.968
+                    }
+                ]
+            }
+        ),
+        OpenApiExample(
+            'Nenhum dispositivo encontrado',
+            value={
+                "status": 200,
+                "msg": "Nenhum dispositivo encontrado num raio de 1.0 km.",
+                "dispositivos": []
+            },
+            response_only=True,
+            status_codes=['200'],
+        ),
+        OpenApiExample(
+            'Parâmetros inválidos',
+            response_only=True,
+            status_codes=['400'],
+            value={
+                "status": 400,
+                "msg": "Parâmetros \"latitude\", \"longitude\" e \"raio\" são obrigatórios e devem ser válidos."
+            }
+        )
+    ]
+)
 class DispositivosProximosRaioView(APIView):
     def get(self, request):
+        # Captura e valida os parâmetros de latitude, longitude e raio
         try:
-            lat = float(request.GET.get('lat'))
-            lon = float(request.GET.get('lon'))
+            latitude = float(request.GET.get('latitude'))
+            longitude = float(request.GET.get('longitude'))
             raio_km = float(request.GET.get('raio'))
         except (TypeError, ValueError):
             return Response({
                 'status': 400,
-                'msg': 'Parâmetros "lat", "lon" e "raio" são obrigatórios e devem ser válidos.'
+                'msg': 'Parâmetros "latitude", "longitude" e "raio" são obrigatórios e devem ser válidos.'
             }, status=400)
 
-        ponto_referencia = Point(lon, lat, srid=4326)
+        # Cria o ponto de referência geográfica
+        ponto_referencia = Point(longitude, latitude, srid=4326)
 
+        # Filtra os dispositivos dentro do raio especificado e anota a distância até o ponto
         dispositivos = Dispositivo.objects.annotate(
             distancia=Distance('localizacao', ponto_referencia)
         ).filter(
-            localizacao__distance_lte=(ponto_referencia, raio_km * 1000)  # metros
+            localizacao__distance_lte=(ponto_referencia, raio_km * 1000)  # raio em metros
         ).order_by('distancia')
 
-        # Monta resposta com distância incluída
+        if not dispositivos.exists():
+            return Response({
+                'status': 200,
+                'msg': f'Nenhum dispositivo encontrado num raio de {raio_km} km.',
+                'dispositivos': []
+            })
+
+        # Serializa os dispositivos e adiciona a distância individual (em km) ao resultado
         dispositivos_data = []
         for dispositivo in dispositivos:
             dispositivo_serializado = DispositivoSerializer(dispositivo).data
             dispositivo_serializado['distancia_km'] = round(dispositivo.distancia.km, 3)
             dispositivos_data.append(dispositivo_serializado)
 
+        # Retorna a resposta com status, mensagem e lista de dispositivos
         return Response({
             'status': 200,
             'msg': f'Dispositivos encontrados num raio de {raio_km} km.',
             'dispositivos': dispositivos_data
         })
+
     
+@extend_schema(
+    description="Retorna a estação mais próxima de uma coordenada geográfica (latitude e longitude), incluindo a distância até ela.",
+    parameters=[
+        OpenApiParameter(
+            name='latitude',
+            type=OpenApiTypes.FLOAT,
+            location=OpenApiParameter.QUERY,
+            required=True,
+            description='Latitude do ponto de referência (ex: -12.3456)'
+        ),
+        OpenApiParameter(
+            name='longitude',
+            type=OpenApiTypes.FLOAT,
+            location=OpenApiParameter.QUERY,
+            required=True,
+            description='Longitude do ponto de referência (ex: -45.6789)'
+        ),
+    ],
+    responses={
+        200: OpenApiTypes.OBJECT,
+        200: OpenApiTypes.OBJECT,
+        400: OpenApiTypes.OBJECT,
+    },
+    examples=[
+        OpenApiExample(
+            name='Estação encontrada',
+            value={
+                "status": 200,
+                "msg": "Estação mais próxima encontrada.",
+                "estacao": {
+                    "id": 2,
+                    "type": "Feature",
+                    "geometry": "SRID=4326;POINT (-8.6789 -8.303)",
+                    "properties": {
+                        "descricao": "Estação ceplac"
+                    }
+                },
+                "distancia_km": 253.298
+            },
+            response_only=True,
+            status_codes=["200"]
+        ),
+        OpenApiExample(
+            name='Parâmetros inválidos',
+            value={
+                "status": 400,
+                "msg": 'Parâmetros "latitude" e "longitude" são obrigatórios e devem ser válidos.'
+            },
+            response_only=True,
+            status_codes=["400"]
+        ),
+        OpenApiExample(
+            name='Nenhuma estação encontrada',
+            value={
+                "status": 200,
+                "msg": "Nenhuma estação encontrada.",
+                "estacao": None,
+                "distancia_km": None
+            },
+            response_only=True,
+            status_codes=["200"]
+        )
+    ]
+)
 class DispositivoMaisProximoView(APIView):
     def get(self, request):
         try:
-            lat = float(request.GET.get('lat'))
-            lon = float(request.GET.get('lon'))
+            latitude = float(request.GET.get('latitude'))
+            longitude = float(request.GET.get('longitude'))
         except (TypeError, ValueError):
             return Response({
                 'status': 400,
-                'msg': 'Parâmetros "lat" e "lon" são obrigatórios e devem ser válidos.'
+                'msg': 'Parâmetros "latitude" e "longitude" são obrigatórios e devem ser válidos.'
             }, status=400)
 
-        ponto_referencia = Point(lon, lat, srid=4326)
+        ponto_referencia = Point(longitude, latitude, srid=4326)
 
         estacao = Dispositivo.objects.annotate(
             distancia=Distance('localizacao', ponto_referencia)
@@ -64,10 +227,14 @@ class DispositivoMaisProximoView(APIView):
                 'status': 200,
                 'msg': 'Estação mais próxima encontrada.',
                 'estacao': serializer.data,
-                'distancia_km': round(distancia_km, 3)  # arredonda para 3 casas decimais
+                'distancia_km': round(distancia_km, 3)
             })
         else:
             return Response({
-                'status': 404,
-                'msg': 'Nenhuma estação encontrada.'
-            }, status=404)
+                'status': 200,
+                'msg': 'Nenhuma estação encontrada.',
+                'estacao': None,
+                'distancia_km': None
+            })
+
+
